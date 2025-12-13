242d39d922d9be94213c04fa7de6140e57fdf031
Thu Trang Tran <trag.ng14@gmail.com>
2025-12-13 22:42:16 +0700

    Remove unused SQL files and add index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+
+<?php
+session_start();
+require_once "app/App.php";
+require_once "app/DB.php";
+require_once "app/Controller.php";
+require_once "app/config.php";
+$App= new App;
+//var_dump($_GET) ;
+
+?>  
